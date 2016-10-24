f340736396a6e98c791481c0a12cb2956ff10519
yuchenghai <yuchenghai@yixin.im>
2016-10-24 20:48:18 +0800

    init project

---
+++ JNAudioKitDemo/JNAudioKitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JNAudioKitDemo
+//
+//  Created by NetEase on 16/10/24.
+//  Copyright © 2016年 NetEase. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
